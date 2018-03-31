073d74032af5631afdff55295cf489fe1a882309
Donald Jones <djones6@ebsco.com>
2018-03-30 19:53:22 -0500

    Updated paths to forked version of repos.

--- enablerepo.sh
+++ enablerepo.sh
@@ -13,12 +13,12 @@ fi
 apt update
 apt upgrade -y
 apt install -y apt-transport-https gnupg wget
-wget -O "${TMPDIR}/pubkey.gpg" https://xeffyr.github.io/termux-x-repository/pubkey.gpg
+wget -O "${TMPDIR}/pubkey.gpg" https://raw.githubusercontent.com/pwdonald/termux-x-repository/master/pubkey.gpg
 apt-key add "${TMPDIR}/pubkey.gpg"
 rm -f "${TMPDIR}/pubkey.gpg"
 
 ## Prevent duplicate entries in sources.list
-sed -i '/https:\/\/xeffyr.github.io\/termux-x-repository/d' "${PREFIX}/etc/apt/sources.list"
+sed -i '/https:\/\/raw.githubusercontent.com\/pwdonald\/termux-x-repository\/master\/termux-x-repository/d' "${PREFIX}/etc/apt/sources.list"
 
 ARCHITECTURE=$(uname -m)
 if [ -z "${ARCHITECTURE}" ]; then
@@ -26,6 +26,6 @@ if [ -z "${ARCHITECTURE}" ]; then
     echo "    Use instructions provided in README.md file."
     exit 1
 fi
-echo "deb [arch=all,${ARCHITECTURE}] https://xeffyr.github.io/termux-x-repository/ termux x-gui" >> "${PREFIX}/etc/apt/sources.list"
+echo "deb [arch=all,${ARCHITECTURE}] https://raw.githubusercontent.com/pwdonald/termux-x-repository/master/ termux x-gui" >> "${PREFIX}/etc/apt/sources.list"
 
 apt update
